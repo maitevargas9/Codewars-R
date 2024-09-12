da9bb2b1651ba652f435a6cc7875f76bb71d89b4
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2024-09-12 17:22:59 +0200

    First commit

---
+++ 8kyu/Convert a Number to a String!.R
@@ -0,0 +1,11 @@
+# Description
+# We need a function that can transform a number (integer) into a string.
+# What ways of achieving this do you know?
+# Examples (input --> output):
+# 123  --> "123"
+# 999  --> "999"
+# -100 --> "-100"
+
+number_to_string <- function(n) {
+  return(toString(n))
+}
